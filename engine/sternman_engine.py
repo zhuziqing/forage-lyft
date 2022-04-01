dd6cb2ad96e7e298c1278f9ea4586e1026556a0b
zhuziqing <364161903@qq.com>
2022-04-01 04:56:13 -0700

    refactor

--- engine/sternman_engine.py
+++ engine/sternman_engine.py
@@ -4,8 +4,7 @@ from car import Car
 
 
 class SternmanEngine(Car, ABC):
-    def __init__(self, last_service_date, warning_light_is_on):
-        super().__init__(last_service_date)
+    def __init__(self, warning_light_is_on):
         self.warning_light_is_on = warning_light_is_on
 
     def engine_should_be_serviced(self):

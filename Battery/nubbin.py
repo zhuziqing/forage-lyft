a39f286c67fb6a012bd5833f641256d73d2f6634
zhuziqing <364161903@qq.com>
2022-04-01 04:47:30 -0700

    refactor

---
+++ Battery/nubbin.py
@@ -0,0 +1,17 @@
+import Battery
+
+class NubbinBattery(Battery):
+   def _init_(self, lastServiceDate, currentDate){
+      self.currentDate = currentDate
+      self.lastServiceDate = lastServiceDate
+
+   def needs_service(self):
+      nextServiceDate = self.calculateDate(self.lastServiceDate,2)
+      if nextServiceDate < self.currentDate:
+           return True
+      else:
+           return False
+
+   def calculateDate(dateToBeAdded, years):
+      result = dateToBeAdded.replace(year = dateToBeAdded.year + years)
+      return result
